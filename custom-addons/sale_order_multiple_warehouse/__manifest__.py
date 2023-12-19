bd9cc1143bfb0825fd46b9c1e7e560a09b808b42
Rimvydas Kanapka <projektu.vadovas@eurofondas.lt>
2023-12-19 15:06:49 +0200

    [ADD] sale_order_multiple_warehouse: initial commit

---
+++ custom-addons/sale_order_multiple_warehouse/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Multiple Warehouse',
+    'version': '1.0',
+    'category': 'Sales/Sales',
+    'sequence': 55,
+    'summary': 'Sale Order Multiple Warehouse',
+    'depends': [
+        'sale_management',
+        'stock'
+    ],
+    'application': True,
+}

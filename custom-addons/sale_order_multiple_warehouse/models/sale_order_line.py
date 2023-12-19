e702b0af28f0d481c01a58e57c2b3cba8ee21938
Rimvydas Kanapka <projektu.vadovas@eurofondas.lt>
2023-12-19 15:10:15 +0200

    [FEAT] sale_order_multiple_warehouse: set default wh for sol

--- custom-addons/sale_order_multiple_warehouse/models/sale_order_line.py
+++ custom-addons/sale_order_multiple_warehouse/models/sale_order_line.py
@@ -13,12 +13,3 @@ class SaleOrderLine(models.Model):
     def onchange_product_warehouse_id(self):
         self.product_warehouse_id = self.warehouse_id
 
-    def _prepare_procurement_values(self, group_id=False):
-        """ Prepare specific key for moves or other components that will be created from a stock rule
-        coming from a sale order line. This method could be override in order to add other custom key that could
-        be used in move/po creation.
-        """
-        values = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
-        if self.product_warehouse_id:
-            values.update({'warehouse_id': self.product_warehouse_id})
-        return values
